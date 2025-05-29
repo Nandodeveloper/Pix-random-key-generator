bbb7063f88304081f0418c7e0e4ed15be4d1e08e
Fernando <fernandinhomoraesbr@gmail.com>
2025-05-29 00:11:25 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank_GeradorChavePix/GeradorPix.cs
@@ -0,0 +1,36 @@
+﻿namespace bytebank_GeradorChavePix;
+
+/// <summary>
+/// Class that generates random PIX keys using the Guid format.
+/// </summary>
+public static class GeradorPix
+{
+    /// <summary>
+    /// Method that returns a random PIX key.
+    /// </summary>
+    /// <returns>Returns a PIX key in String format.</returns>
+    public static string GetChavePix() => Guid.NewGuid().ToString();
+
+    /// <summary>
+    /// Method that returns a list of random PIX keys.
+    /// </summary>
+    /// <param name="numeroChaves"> Number of keys to be generated.</param>
+    /// <returns>List of PIX key strings.</returns>
+    public static List<string> GetChavesPix(int numeroChaves) 
+    {
+        if (numeroChaves <= 0)
+        {
+            return null;
+        }
+        else
+        {
+            var chaves = new List<string>();
+            for (int i = 0; i < numeroChaves; i++)
+            {
+                chaves.Add(Guid.NewGuid().ToString());
+            }
+
+            return chaves;
+        } 
+    }
+}
